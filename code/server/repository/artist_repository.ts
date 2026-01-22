f3341ed4650049ecb90a5fa73007f398fb25fd1d
bibidevv <abissetou.tounkara@hotmail.com>
2026-01-22 15:07:01 +0100

    repository ok

--- code/server/repository/artist_repository.ts
+++ code/server/repository/artist_repository.ts
@@ -1,4 +1,4 @@
-import type { QueryResult } from "mysql2";
+import type { QueryResult, RowDataPacket } from "mysql2";
 import type { Artists } from "../../models/artists";
 import type { Origins } from "../../models/origins";
 import type { Styles } from "../../models/styles";
@@ -14,38 +14,38 @@ class ArtistRepository {
 		const connection = await new MySQLService().connect();
 
 		const sql = `
-			SELECT ${this.table}.*,
-				GROUP_CONCAT(style.id) AS styles_ids,
-				GROUP_CONCAT(origin.id) AS origins_ids
-			FROM ${process.env.MYSQL_DATABASE}.${this.table}
-			LEFT JOIN ${process.env.MYSQL_DATABASE}.artist_style
-				ON ${this.table}.id = artist_style.artist_id
-			LEFT JOIN ${process.env.MYSQL_DATABASE}.style
-				ON style.id = artist_style.style_id
-			LEFT JOIN ${process.env.MYSQL_DATABASE}.artist_origin
-				ON ${this.table}.id = artist_origin.artist_id
-			LEFT JOIN ${process.env.MYSQL_DATABASE}.origin
-				ON artist_origin.origin_id = origin.id
-			GROUP BY ${this.table}.id;
-		`;
+      SELECT ${this.table}.*,
+             GROUP_CONCAT(style.id) AS styles_ids,
+             GROUP_CONCAT(origin.id) AS origins_ids
+      FROM ${process.env.MYSQL_DATABASE}.${this.table}
+      LEFT JOIN ${process.env.MYSQL_DATABASE}.artist_style
+        ON ${this.table}.id = artist_style.artist_id
+      LEFT JOIN ${process.env.MYSQL_DATABASE}.style
+        ON style.id = artist_style.style_id
+      LEFT JOIN ${process.env.MYSQL_DATABASE}.artist_origin
+        ON ${this.table}.id = artist_origin.artist_id
+      LEFT JOIN ${process.env.MYSQL_DATABASE}.origin
+        ON origin.id = artist_origin.origin_id
+      GROUP BY ${this.table}.id;
+    `;
 
 		try {
-			const [query] = await connection.execute(sql);
+			const [rows] = await connection.execute<RowDataPacket[]>(sql);
 
-			for (let i = 0; i < (query as Artists[]).length; i++) {
-				const result = (query as Artists[])[i] as Artists;
+			const artists = rows as unknown as Artists[];
 
-				result.styles = (await new StyleRepository().selectInList(
-					result.styles_ids as string,
+			for (const artist of artists) {
+				artist.styles = (await new StyleRepository().selectInList(
+					artist.styles_ids as string,
 				)) as Styles[];
 
-				result.origins = (await new OriginsRepository().selectInList(
-					result.origins_ids as string,
+				artist.origins = (await new OriginsRepository().selectInList(
+					artist.origins_ids as string,
 				)) as Origins[];
 			}
 
-			return query as Artists[];
-		} catch (error) {
+			return artists;
+		} catch {
 			return [];
 		}
 	};
@@ -57,34 +57,34 @@ class ArtistRepository {
 		const connection = await new MySQLService().connect();
 
 		const sql = `
-			SELECT ${this.table}.*,
-				GROUP_CONCAT(style.id) AS styles_ids,
-				GROUP_CONCAT(origin.id) AS origins_ids
-			FROM ${process.env.MYSQL_DATABASE}.${this.table}
-			LEFT JOIN ${process.env.MYSQL_DATABASE}.artist_style
-				ON ${this.table}.id = artist_style.artist_id
-			LEFT JOIN ${process.env.MYSQL_DATABASE}.artist_origin
-				ON ${this.table}.id = artist_origin.artist_id
-			WHERE ${this.table}.id = ?
-			GROUP BY ${this.table}.id;
-		`;
+      SELECT ${this.table}.*,
+             GROUP_CONCAT(style.id) AS styles_ids,
+             GROUP_CONCAT(origin.id) AS origins_ids
+      FROM ${process.env.MYSQL_DATABASE}.${this.table}
+      LEFT JOIN ${process.env.MYSQL_DATABASE}.artist_style
+        ON ${this.table}.id = artist_style.artist_id
+      LEFT JOIN ${process.env.MYSQL_DATABASE}.artist_origin
+        ON ${this.table}.id = artist_origin.artist_id
+      WHERE ${this.table}.id = ?
+      GROUP BY ${this.table}.id;
+    `;
 
 		try {
-			const [query] = await connection.execute(sql, [data.id]);
-			const result = (query as Artists[]).shift();
+			const [rows] = await connection.execute<RowDataPacket[]>(sql, [data.id]);
+			const artist = (rows as unknown as Artists[])[0];
 
-			if (!result) return null;
+			if (!artist) return null;
 
-			result.styles = (await new StyleRepository().selectInList(
-				result.styles_ids as string,
+			artist.styles = (await new StyleRepository().selectInList(
+				artist.styles_ids as string,
 			)) as Styles[];
 
-			result.origins = (await new OriginsRepository().selectInList(
-				result.origins_ids as string,
+			artist.origins = (await new OriginsRepository().selectInList(
+				artist.origins_ids as string,
 			)) as Origins[];
 
-			return result;
-		} catch (error) {
+			return artist;
+		} catch {
 			return null;
 		}
 	};
@@ -95,55 +95,47 @@ class ArtistRepository {
 	): Promise<QueryResult | null> => {
 		const connection = await new MySQLService().connect();
 
-		const sql = `INSERT INTO ${process.env.MYSQL_DATABASE}.${this.table} 
-			(id, name, description, image, bio) VALUES (NULL, :name, :description, :image, :bio);`;
+		let sql = `
+      INSERT INTO ${process.env.MYSQL_DATABASE}.${this.table} 
+      (name, description, image, bio) VALUES (:name, :description, :image, :bio);
+    `;
 
 		try {
 			await connection.beginTransaction();
 
+			// Insert artiste
 			await connection.execute(sql, data);
 
-			// deuxième rêquete SQL
-			sql = `SET @id = LAST_INSERT_ID();`;
-			await connection.execute(sql, data);
-			// troixième rêquete
-			// INSERT INTO codefilles.inspirations
-			// VALUES
-			// (1,@id),
-			// (2,@id),
-			// (3,@id);
-			// split : extraire les données d'une chaîne de caractéres en array
-			//  1,2,3 >>[1,2,3]
-			// map[1,2,3] >> [(1,@id), (2,@id),(3,@id)]
-			// join[(1,@id), (2,@id),(3,@id))]
+			// Récupérer le dernier ID inséré
+			const [rows] = await connection.execute<RowDataPacket[]>(
+				`SELECT LAST_INSERT_ID() AS insertId;`,
+			);
+			const insertId = rows[0].insertId as number;
 
-			// Hydrater artist_style
+			// Insert dans artist_style
 			if (data.styles_ids) {
-				const joinsIds = (data.styles_ids as string)
+				const joinsStyles = (data.styles_ids as string)
 					.split(",")
-					.map((value) => `(${value}, ${insertId})`)
+					.map((id) => `(${id}, ${insertId})`)
 					.join(",");
+				sql = `INSERT INTO ${process.env.MYSQL_DATABASE}.artist_style (style_id, artist_id) VALUES ${joinsStyles};`;
+				await connection.execute(sql);
+			}
 
-			// execution de la rêquete
-			// si la rêquete posséde des variables, utiliser le paramètre de la méthode
-			sql = ` insert into ${process.env.MYSQL_DATABASE}.artist_styles value ${joinsIds};`;
-
-			// autre
-			await connection.execute(sql, data);
-
-			const joinsIds1 = (data.origins_ids as string)
-				?.split(",")
-				.map((value) => `(${value}, @id)`)
-				.join();
-
-			sql = ` insert into ${process.env.MYSQL_DATABASE}.artist_origins value ${joinsIds1};`;
-
-			const [query] = await connection.execute(sql);
+			// Insert dans artist_origin
+			if (data.origins_ids) {
+				const joinsOrigins = (data.origins_ids as string)
+					.split(",")
+					.map((id) => `(${id}, ${insertId})`)
+					.join(",");
+				sql = `INSERT INTO ${process.env.MYSQL_DATABASE}.artist_origin (origin_id, artist_id) VALUES ${joinsOrigins};`;
+				await connection.execute(sql);
+			}
 
 			await connection.commit();
 
 			return { insertId } as unknown as QueryResult;
-		} catch (error) {
+		} catch {
 			await connection.rollback();
 			return null;
 		}
@@ -156,15 +148,15 @@ class ArtistRepository {
 		const connection = await new MySQLService().connect();
 
 		const sql = `
-			UPDATE ${process.env.MYSQL_DATABASE}.${this.table} 
-			SET name = :name, image = :image, bio = :bio
-			WHERE id = :id;
-		`;
+      UPDATE ${process.env.MYSQL_DATABASE}.${this.table} 
+      SET name = :name, image = :image, bio = :bio
+      WHERE id = :id;
+    `;
 
 		try {
 			const [query] = await connection.execute(sql, data);
 			return query as QueryResult;
-		} catch (error) {
+		} catch {
 			return null;
 		}
 	};
@@ -178,7 +170,7 @@ class ArtistRepository {
 		try {
 			await connection.beginTransaction();
 
-			// Supprimer les relations dans artist_style et artist_origin
+			// Supprimer les relations
 			await connection.execute(
 				`DELETE FROM ${process.env.MYSQL_DATABASE}.artist_style WHERE artist_id = :id;`,
 				data,
@@ -195,9 +187,8 @@ class ArtistRepository {
 			);
 
 			await connection.commit();
-
 			return query as QueryResult;
-		} catch (error) {
+		} catch {
 			await connection.rollback();
 			return null;
 		}
